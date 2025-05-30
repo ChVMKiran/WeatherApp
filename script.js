0a45f3454cc0c3cab38219f75e5b1c93904b76b6
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2025-05-30 13:15:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const apikey = "7b59f1de489649b185e65138253005";
+const url = 'https://api.weatherapi.com/v1/current.json';
+
+const searchButton = document.getElementById("searchbutton");
+
+
+async function getWeather(city) {
+    const response = await fetch(`${url}?key=${apikey}&q=${city}`);
+    const data = await response.json();
+    // console.log(data.current);
+    document.querySelector(".weatherInfo").style.display = "block";
+    if (response.ok) {
+        document.getElementById("cityName").innerText = data.location.name;
+        document.getElementById("temperature").innerText = `${data.current.temp_c}`;
+        document.getElementById("condition").innerText = data.current.condition.text;
+        document.getElementById("weatherIcon").src = `https:${data.current.condition.icon}`;
+        document.getElementById("feelslike").innerText = `${data.current.feelslike_c}`;
+        document.getElementById("windSpeed").innerText = `${data.current.wind_kph}`;
+        document.getElementById("humidity").innerText = `${data.current.humidity}`;
+        document.getElementById("precipitation").innerText = `${data.current.precip_mm}`;
+    }
+     else {
+        alert("City not found. Please try again.");
+        window.location.reload();
+    }
+}
+
+
+searchButton.addEventListener("click", () => {
+    getWeather(document.getElementById("cityInput").value);
+});
+
+document.getElementById("cityInput").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+       searchButton.click();
+    }
+});
